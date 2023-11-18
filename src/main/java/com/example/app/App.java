5b007ed8f6f7882345b92e40930da9845e32e5c2
bvuongle <01184762@pw.edu.pl>
2023-11-18 15:49:06 +0100

    Add DB dependencies and connect with test.db

--- src/main/java/com/example/app/App.java
+++ src/main/java/com/example/app/App.java
@@ -1,5 +1,7 @@
 package main.java.com.example.app;
 import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -9,39 +11,27 @@ import java.sql.ResultSet;
 import main.java.com.example.app.ui.MainPage;
 
 public class App {
-    public static void main(String[] args) throws IOException {
-        System.out.println("Hello");
-        String jdbcUrl = "jdbc:sqlite:test.db";
-        try (Connection connection = DriverManager.getConnection(jdbcUrl);
-             PreparedStatement createTableStatement = connection.prepareStatement(
-                     "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT)");
-             PreparedStatement insertStatement = connection.prepareStatement(
-                     "INSERT INTO users (name) VALUES (?)");
-             PreparedStatement selectStatement = connection.prepareStatement(
-                     "SELECT * FROM users")) {
-
-            // Create table if not exists
-            createTableStatement.execute();
-
-            // Insert data
-            insertStatement.setString(1, "John Doe");
-            insertStatement.executeUpdate();
-            insertStatement.setString(1, "Jane Smith");
-            insertStatement.executeUpdate();
-
-            System.out.println("Database created successfully.");
-
-            // Query data
-            ResultSet resultSet = selectStatement.executeQuery();
-            System.out.println("Users in the database:");
-            while (resultSet.next()) {
-                int userId = resultSet.getInt("id");
-                String userName = resultSet.getString("name");
-                System.out.println("User ID: " + userId + ", Name: " + userName);
-            }
-
+    private Connection connect() {
+        // test.db is in src/main/java/com/example/app/database, and App is in src/main/java/com/example/app
+        String url = "jdbc:sqlite:src/main/java/com/example/app/database/test.db";
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url);
         } catch (SQLException e) {
-            e.printStackTrace();
+            System.out.println(e.getMessage());
         }
+        return conn;
+    }
+    public void connectDB() {
+        try(Connection conn = this.connect()) {
+            System.out.println("Connection to SQLite has been established.");
+        }
+        catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        App app = new App();
+        app.connectDB();
     }
 }
