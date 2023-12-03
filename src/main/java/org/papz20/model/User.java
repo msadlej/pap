7fa5dfe1bb6249ed6506c93f46be5b5b7a1bb370
msadlej <01178587@pw.edu.pl>
2023-12-03 22:10:07 +0100

    add exceptions

--- src/main/java/org/papz20/model/User.java
+++ src/main/java/org/papz20/model/User.java
@@ -50,33 +50,43 @@ public abstract class User {
         this.id = id;
     }
 
-    public void setFirstName(String firstName) {
+    public void setFirstName(String firstName) throws IllegalArgumentException{
         if (!firstName.isEmpty() && firstName.length() < 30) {
             this.first_name = firstName;
+        } else {
+            throw new IllegalArgumentException("First name cannot be empty or over 30 characters");
         }
     }
 
-    public void setLastName(String lastName) {
+    public void setLastName(String lastName) throws IllegalArgumentException{
         if (!lastName.isEmpty() && lastName.length() < 30) {
             this.last_name = lastName;
+        } else {
+            throw new IllegalArgumentException("Last name cannot be empty or over 30 characters");
         }
     }
 
-    public void setEmail(String email) {
+    public void setEmail(String email) throws IllegalArgumentException{
         if (!email.isEmpty() && email.length() < 30) {
             this.email = email;
+        } else {
+            throw new IllegalArgumentException("Email cannot be empty or over 30 characters");
         }
     }
 
-    public void setUsername(String username) {
+    public void setUsername(String username) throws IllegalArgumentException{
         if (!username.isEmpty() && username.length() < 30) {
             this.username = username;
+        } else {
+            throw new IllegalArgumentException("Username cannot be empty or over 30 characters");
         }
     }
 
-    public void setPassword(String password) {
+    public void setPassword(String password) throws IllegalArgumentException{
         if (!password.isEmpty() && password.length() < 30) {
             this.password = password;
+        } else {
+            throw new IllegalArgumentException("Password cannot be empty or over 30 characters");
         }
     }
 }
