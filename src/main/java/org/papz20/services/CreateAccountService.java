85a231a2f9eebbfcf9a11c06585f5edaf81411bc
Binh Vuong Le Duc <01184762@pw.edu.pl>
2023-12-04 20:38:16 +0100

    Create account service

---
+++ src/main/java/org/papz20/services/CreateAccountService.java
@@ -0,0 +1,46 @@
+package main.java.org.papz20.services;
+
+import main.java.org.papz20.model.Database;
+import main.java.org.papz20.model.User;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class CreateAccountService {
+    private Database database;
+
+    public CreateAccountService() {
+        this.database = new Database();
+    }
+
+    private boolean isUsernameTaken(String username) {
+        String query = "SELECT * FROM users WHERE username = ?";
+        try (
+                Connection connection = database.connectDB();
+                PreparedStatement preparedStatement = connection.prepareStatement(query)
+        ) {
+            preparedStatement.setString(1, username);
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                return resultSet.next();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean createAccount(String first_name, String last_name, String email, String username, String password, String user_type) {
+        if (isUsernameTaken(username)) {
+            return false;
+        } else {
+            try {
+                database.addUser(username, password, first_name, last_name, email, user_type);
+                return true;
+            } catch (Exception e) {
+                e.printStackTrace();
+                return false;
+            }
+        }
+    }
+}
