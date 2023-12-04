d47631b0845e190c8c44a9d8e30a7522e0053100
Wojciech Sarwinski <01133073@pw.edu.pl>
2023-12-04 20:41:43 +0100

    Deleted unused import

--- src/main/java/org/papz20/ui/MainFrame.java
+++ src/main/java/org/papz20/ui/MainFrame.java
@@ -4,7 +4,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.stream.StreamSupport;
 
 public class MainFrame extends JFrame {
     private JPanel main_card_panel;
