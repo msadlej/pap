be7be7af898962af92c666f04fdea4a9bf0f3174
Wojciech Sarwinski <01133073@pw.edu.pl>
2023-12-05 13:50:59 +0100

    Implemented usage of UserAuthenticationPanel

--- src/main/java/org/papz20/ui/MainFrame.java
+++ src/main/java/org/papz20/ui/MainFrame.java
@@ -8,72 +8,22 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class MainFrame extends JFrame {
-    private JPanel main_card_panel;
-    private CardLayout main_card_layout;
-    private LoginPanel login_panel;
-    private CreateAccountPanel create_account_panel;
-    private AuthenticationService authentication_service;
+    private final JPanel main_card_panel;
+    private final CardLayout main_card_layout;
+    private UserAuthenticationPanel user_authentication_panel;
 
     public MainFrame() {
         main_card_panel = new JPanel();
         main_card_layout = new CardLayout();
         main_card_panel.setLayout(main_card_layout);
-        authentication_service = new AuthenticationService();
+        user_authentication_panel = new UserAuthenticationPanel();
 
     }
-
     public void init() {
         setContentPane(main_card_panel);
-        initLoginPanel();
-        initCreateAccountPanel();
-    }
-
-    private void initLoginPanel()
-    {
-        ActionListener login_listener = new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                //TODO: Handle loging in
-                String username = login_panel.getUsername();
-                String password = login_panel.getPassword();
-                System.out.format("Login: %s\nPassword: %s\n", username, password);
-                boolean isAuthenticated = authentication_service.authenticateUser(username, password);
-                if (isAuthenticated) {
-                    System.out.println("Login successful!");
-                }
-                else {
-                    System.out.println("Invalid username or password!");
-                }
-            }
-        };
-        ActionListener account_creation_listener = new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                login_panel.clearFields();
-                main_card_layout.show(main_card_panel, "CreateAccount");
-            }
-        };
-        login_panel = new LoginPanel(login_listener, account_creation_listener);
-        main_card_panel.add(login_panel, "Login");
-    }
-
-    private void initCreateAccountPanel()
-    {
-        ActionListener account_creation_listener = new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                //TODO: Handle creating account
-                System.out.println("Account creation requested!");
-            }
-        };
-        ActionListener back_listener = new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                main_card_layout.show(main_card_panel, "Login");
-            }
-        };
-        create_account_panel = new CreateAccountPanel(account_creation_listener, back_listener);
-        main_card_panel.add(create_account_panel, "CreateAccount");
+        user_authentication_panel.init();
+        main_card_panel.add(user_authentication_panel, "authentication");
+        main_card_layout.show(main_card_panel, "authentication");
     }
 
     public static void main(String[] args) {
