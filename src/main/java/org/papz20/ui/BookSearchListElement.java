b68f0a4d15ea6f898f0baf7b3407286d202e768d
Wojciech Sarwinski <01133073@pw.edu.pl>
2023-12-31 00:21:25 +0100

    Created BookSearchListElement skeleton

---
+++ src/main/java/org/papz20/ui/BookSearchListElement.java
@@ -0,0 +1,16 @@
+package main.java.org.papz20.ui;
+
+import javax.swing.*;
+
+public class BookSearchListElement extends JPanel {
+    private JLabel book_title;
+    private JLabel book_author;
+    private JButton orderButton;
+    private JLabel book_availability;
+    private JLabel book_genre;
+
+    public BookSearchListElement(String title, String author, String genre)
+    {
+        // TODO: implement changing of fields and add functionality
+    }
+}
