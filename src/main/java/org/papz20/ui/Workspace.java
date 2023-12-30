ad4b43067a7837934088e1b4757d043982619058
Wojciech Sarwinski <01133073@pw.edu.pl>
2023-12-31 00:31:34 +0100

    Created Workspace skeleton

---
+++ src/main/java/org/papz20/ui/Workspace.java
@@ -0,0 +1,11 @@
+package main.java.org.papz20.ui;
+
+import javax.swing.*;
+
+public class Workspace extends JFrame {
+    private JButton search;
+    private JButton myBooks;
+    private JButton myAccount;
+    private JButton logOut;
+    private JPanel side_panel;
+}
