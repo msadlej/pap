c4596e21c100226e97b85f91b4c0e6004e5d2248
msadlej <01178587@pw.edu.pl>
2024-01-06 19:18:46 +0100

    add Order getters and setters

--- src/main/java/org/papz20/model/Order.java
+++ src/main/java/org/papz20/model/Order.java
@@ -16,4 +16,61 @@ public class Order {
         setPeriod(period);
         setStatus(status);
     }
+
+
+    public int getId() {
+        return this.id;
+    }
+
+    public User getUser() {
+        return this.user;
+    }
+
+    public Copy getCopy() {
+        return this.copy;
+    }
+
+    public String getDate() {
+        return this.date;
+    }
+
+    public int getPeriod() {
+        return this.period;
+    }
+
+    public String getStatus() {
+        return this.status;
+    }
+
+    public void setId(int id) throws IllegalArgumentException {
+        if (id > 0) {
+            this.id = id;
+        } else {
+            throw new IllegalArgumentException("Order id must be a positive integer.");
+        }
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public void setCopy(Copy copy) {
+        this.copy = copy;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void setPeriod(int period) throws IllegalArgumentException {
+        if (period > 0) {
+            this.period = period;
+        } else {
+            throw new IllegalArgumentException("Period must be a positive integer.");
+        }
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
