9bdec77c0ac03e34f29012aab4bd838ddea812c3
msadlej <01178587@pw.edu.pl>
2024-01-06 19:24:18 +0100

    add Fine getters and setters

--- src/main/java/org/papz20/model/Fine.java
+++ src/main/java/org/papz20/model/Fine.java
@@ -12,4 +12,44 @@ public class Fine {
         setAmount(amount);
         setStatus(status);
     }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public Transaction getTransaction() {
+        return this.transaction;
+    }
+
+    public int getAmount() {
+        return this.amount;
+    }
+
+    public String getStatus() {
+        return this.status;
+    }
+
+    public void setId(int id) throws IllegalArgumentException {
+        if (id > 0) {
+            this.id = id;
+        } else {
+            throw new IllegalArgumentException("Fine id must be a positive integer.");
+        }
+    }
+
+    public void setTransaction(Transaction transaction) {
+        this.transaction = transaction;
+    }
+
+    public void setAmount(int amount) throws IllegalArgumentException {
+        if (amount > 0) {
+            this.amount = amount;
+        } else {
+            throw new IllegalArgumentException("Fine amount must be a positive integer.");
+        }
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
