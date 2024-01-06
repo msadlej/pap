0930ec084047cc6aca656223dbfbde0f28c0d8ff
msadlej <01178587@pw.edu.pl>
2024-01-06 19:12:38 +0100

    add Copy getters and setters

--- src/main/java/org/papz20/model/Copy.java
+++ src/main/java/org/papz20/model/Copy.java
@@ -10,4 +10,32 @@ public class Copy {
         setBook(book);
         setAvailable(available);
     }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public Book getBook() {
+        return this.book;
+    }
+
+    public boolean getAvailable() {
+        return this.available;
+    }
+
+    public void setId(int id) throws IllegalArgumentException {
+        if (id > 0) {
+            this.id = id;
+        } else {
+            throw new IllegalArgumentException("Copy id must be a positive integer.");
+        }
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
 }
