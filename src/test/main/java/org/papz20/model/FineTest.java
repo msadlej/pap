bc417b5781ddfa982cb9d11efe89a84eb81eb234
msadlej <01178587@pw.edu.pl>
2024-01-09 13:20:23 +0100

    update models Tests

--- src/test/main/java/org/papz20/model/FineTest.java
+++ src/test/main/java/org/papz20/model/FineTest.java
@@ -4,19 +4,15 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class FineTest {
-    private User user = new User(1, "John", "Smith", "john.smith@gmail.com", "jsmith", "password");
-    private Book book = new Book(1, "The Hobbit", "J.R.R. Tolkien", "Fantasy", "21/09/1937");
-    private Copy copy = new Copy(1, book, true);
-    private Transaction transaction = new Transaction(1, user, copy, "21/09/2021", "21/10/2021", "checked out");
-    private Fine fine = new Fine(1, transaction, 100, "unpaid");
+    private Fine fine = new Fine(1, 1, 100, "unpaid");
     @Test
     public void testGetId() {
         Assertions.assertEquals(1, fine.getId());
     }
 
     @Test
-    public void testGetTransaction() {
-        Assertions.assertEquals(transaction, fine.getTransaction());
+    public void testGetTransactionId() {
+        Assertions.assertEquals(1, fine.getTransactionId());
     }
 
     @Test
@@ -36,10 +32,9 @@ public class FineTest {
     }
 
     @Test
-    public void testSetTransaction() {
-        Transaction transaction1 = new Transaction(2, user, copy, "21/09/2021", "21/10/2021", "checked out");
-        fine.setTransaction(transaction1);
-        Assertions.assertEquals(transaction1, fine.getTransaction());
+    public void testSetTransactionId() {
+        fine.setTransactionId(2);
+        Assertions.assertEquals(2, fine.getTransactionId());
     }
 
     @Test
@@ -58,6 +53,13 @@ public class FineTest {
     public void testSetInvalidId() {
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
             fine.setId(-1);
+        });
+    }
+
+    @Test
+    public void testSetInvalidTransactionId() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            fine.setTransactionId(-1);
         });
     }
 
