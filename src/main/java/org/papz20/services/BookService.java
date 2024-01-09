6543bde14c0653317beb482f836b79f6ad0b2af3
msadlej <01178587@pw.edu.pl>
2024-01-09 22:18:49 +0100

    add BookService

---
+++ src/main/java/org/papz20/services/BookService.java
@@ -0,0 +1,31 @@
+package main.java.org.papz20.services;
+
+import main.java.org.papz20.model.Database;
+import main.java.org.papz20.model.Book;
+import java.sql.SQLException;
+
+public class BookService {
+    private Database database;
+
+    public BookService() { this.database = new Database(); }
+
+    private void addBook(Book book) {
+        database.addBook(book);
+    }
+
+    private void addBook(int bookId, String title, String author, String genre, String publishDate) {
+        database.addBook(bookId, title, author, genre, publishDate);
+    }
+
+    private void addBook(String title, String author, String genre, String publishDate) {
+        database.addBook(title, author, genre, publishDate);
+    }
+
+    private void removeBook(Book book) {
+        database.removeBook(book);
+    }
+
+    private void removeBook(int bookId) {
+        database.removeBook(bookId);
+    }
+}
