5e4439933950d20599c3aa3720fb4042a52e3b47
Jakub Wróblewski <wroblewskijakub12@gmail.com>
2024-01-10 09:41:02 +0100

    skeleton penalty service

---
+++ src/main/java/org/papz20/services/AddPenaltyToMemberService.java
@@ -0,0 +1,20 @@
+package main.java.org.papz20.services;
+
+import main.java.org.papz20.model.Database;
+
+public class AddPenaltyToMemberService {
+    private Database database;
+
+    public AddPenaltyToMemberService() { this.database = new Database(); }
+
+    public boolean addPenalty(String username, int transaction_id, int penalty_amount){
+        int user_id =
+        ///('unpaid', 'paid')
+        return false;
+    }
+
+    public boolean makePenaltyPaid(){
+        return false;
+    }
+
+}
