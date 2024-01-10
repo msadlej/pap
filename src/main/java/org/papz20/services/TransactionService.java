e8c3c9510b93e996ecf11e69eea71e19abc101e3
Jakub Wróblewski <wroblewskijakub12@gmail.com>
2024-01-10 21:07:16 +0100

    createTransactionFromOrder as requested

--- src/main/java/org/papz20/services/TransactionService.java
+++ src/main/java/org/papz20/services/TransactionService.java
@@ -4,6 +4,7 @@ import main.java.org.papz20.model.Database;
 import main.java.org.papz20.model.Order;
 import main.java.org.papz20.model.Transaction;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class TransactionService {
     private Database database;
@@ -12,15 +13,23 @@ public class TransactionService {
 
     public Transaction createTransactionFromOrder(int order_id){
         Transaction this_transaction = null;
-        int transaction_id = database.getNextId("transactions");
-        Order this_order = database.fetchOrder(order_id);
-        int user_id = this_order.getUserId();
-        int copy_id = this_order.getCopyId();
-        String checkout_date = this_order.getDate();
-        int days_count = this_order.getPeriod();
-        LocalDate parsedCheckoutDate = LocalDate.parse(checkout_date, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-        LocalDate dueDate = parsedCheckoutDate.plusDays(daysToAdd);
 
+        try{
+            int transaction_id = database.getNextId("transactions");
+            Order this_order = database.fetchOrder(order_id);
+            int user_id = this_order.getUserId();
+            int copy_id = this_order.getCopyId();
+            String checkout_date = this_order.getDate();
+            int days_count = this_order.getPeriod();
+            LocalDate parsed_checkout_date = LocalDate.parse(checkout_date, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+            LocalDate parsed_due_date = parsed_checkout_date.plusDays(days_count);
+            String due_date = parsed_due_date.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+
+            this_transaction = new Transaction(transaction_id, order_id, user_id, copy_id, checkout_date, due_date, "checked out");
+            database.addTransaction(this_transaction);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
 
         return this_transaction;
     }
