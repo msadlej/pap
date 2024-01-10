9617204d66c3032fc914b329e292ccfda64f63a2
Wojciech Sarwinski <01133073@pw.edu.pl>
2024-01-10 19:23:48 +0100

    Implemented ManageMembers panel without functionalities

--- src/main/java/org/papz20/ui/ManageMembers.java
+++ src/main/java/org/papz20/ui/ManageMembers.java
@@ -28,11 +28,15 @@ public class ManageMembers extends JPanel{
         ActionListener select_listener = new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                clear();
                 // Load data into card and set result as selected user
-                // TODO: check if valid user id ie. valid input, database returns valid user.
+                // TODO: check if valid user id ie.
+                //  valid input,
+                //  database returns valid user.
+                //  if not return early
                 delete_account.setVisible(true);
                 int user_id = Integer.parseInt(user_id_field.getText());
-                selected_user = new Database().fetchUser(user_id);
+                selected_user = new Database().fetchUser(user_id); // TODO: check if valid result
                 user_card.setVisible(true);
                 name_surname.setText(String.format("name surname: %s %s", selected_user.getFirstName(), selected_user.getLastName()));
                 username.setText(String.format("username: %s", selected_user.getUsername()));
@@ -45,7 +49,10 @@ public class ManageMembers extends JPanel{
         ActionListener delete_listener = new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                // TODO: check if valid user id ie. valid input, database returns valid user and user cannot delete self.
+                // TODO: check if valid user id ie.
+                //  not null,
+                //  user doesnt have unpaid penalties
+                //  or not returned books and user cannot delete self.
                 //new Database().removeUser(selected_user.getId());
                 clear();
             }
@@ -64,5 +71,7 @@ public class ManageMembers extends JPanel{
         role.setText("role:");
         delete_account.setVisible(false);
     }
+
+    // TODO: add change password text field and button
 
 }
