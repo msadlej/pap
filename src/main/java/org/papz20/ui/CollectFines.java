1b4a9908aeaca99f588801b007b14b3221b485ec
Wojciech Sarwinski <01133073@pw.edu.pl>
2024-01-10 20:37:29 +0100

    Finished CollectFines

--- src/main/java/org/papz20/ui/CollectFines.java
+++ src/main/java/org/papz20/ui/CollectFines.java
@@ -1,10 +1,15 @@
 package main.java.org.papz20.ui;
 
+import main.java.org.papz20.model.Database;
+import main.java.org.papz20.model.Fine;
+import main.java.org.papz20.services.FineService;
+
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.List;
 
 public class CollectFines extends JPanel {
     private JPanel main_panel;
@@ -12,6 +17,7 @@ public class CollectFines extends JPanel {
     private JButton get_fines;
     private JTable fine_table;
     private DefaultTableModel fine_model;
+    private List<Fine> fines;
     private JButton collect_fine;
 
     public CollectFines() {
@@ -28,7 +34,19 @@ public class CollectFines extends JPanel {
         ActionListener get_fines_listener = new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                // TODO: get user id, request fines, add to model
+                int user_id = Integer.parseInt(user_id_field.getText());
+                fines = new Database().viewFines(user_id);
+                for (Fine fine : fines) {
+                    int gr = fine.getAmount() % 100;
+                    int zl = fine.getAmount() / 100;
+                    fine_model.addRow(
+                            new String[]{
+                                    String.valueOf(fine.getId()),
+                                    String.format("%d.%02d PLN", zl, gr),
+                                    fine.getStatus()
+                            }
+                    );
+                }
             }
         };
         get_fines.addActionListener(get_fines_listener);
@@ -36,7 +54,12 @@ public class CollectFines extends JPanel {
         ActionListener collect_fine_listener = new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                // TODO: set fines as paid, clear model
+                FineService service = new FineService();
+                int i = 0;
+                for (Fine fine : fines) {
+                    service.setPaid(fine);
+                    fine_model.setValueAt("paid", i++, 2);
+                }
             }
         };
         collect_fine.addActionListener(collect_fine_listener);
