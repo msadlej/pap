4ac5fe065347b9fb5f08d3b083b55fe994c85eb9
Jakub Wróblewski <wroblewskijakub12@gmail.com>
2024-01-10 20:32:30 +0100

    TransactionService skeleton

---
+++ src/main/java/org/papz20/services/TransactionService.java
@@ -0,0 +1,19 @@
+package main.java.org.papz20.services;
+
+import main.java.org.papz20.model.Database;
+import main.java.org.papz20.model.Order;
+import main.java.org.papz20.model.Transaction;
+
+public class TransactionService {
+    private Database database;
+
+    public TransactionService() { this.database = new Database(); }
+
+    public Transaction createTransactionFromOrder(){
+        Transaction this_transaction = null;
+
+
+
+    }
+
+}
