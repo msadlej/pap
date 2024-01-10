da516c19918df173b3085fabec41f1902c21789e
Wojciech Sarwinski <01133073@pw.edu.pl>
2024-01-10 21:04:39 +0100

    Finished ManageMembers

--- src/main/java/org/papz20/ui/ManageMembers.java
+++ src/main/java/org/papz20/ui/ManageMembers.java
@@ -2,9 +2,9 @@ package main.java.org.papz20.ui;
 
 import main.java.org.papz20.model.Database;
 import main.java.org.papz20.model.User;
+import main.java.org.papz20.services.ChangeUserInfoService;
 
 import javax.swing.*;
-import javax.xml.crypto.Data;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -19,6 +19,10 @@ public class ManageMembers extends JPanel{
     private JLabel username;
     private JLabel email;
     private JLabel role;
+    private JButton change_password;
+    private JTextField new_password_field;
+    private JLabel new_password_prompt;
+    private JLabel deletion_prompt;
     private User selected_user;
 
     public ManageMembers() {
@@ -30,14 +34,13 @@ public class ManageMembers extends JPanel{
             public void actionPerformed(ActionEvent e) {
                 clear();
                 // Load data into card and set result as selected user
-                // TODO: check if valid user id ie.
-                //  valid input,
-                //  database returns valid user.
-                //  if not return early
-                delete_account.setVisible(true);
                 int user_id = Integer.parseInt(user_id_field.getText());
                 selected_user = new Database().fetchUser(user_id); // TODO: check if valid result
-                user_card.setVisible(true);
+                if (selected_user == null) {
+                    clear();
+                    return;
+                }
+                showComponents();
                 name_surname.setText(String.format("name surname: %s %s", selected_user.getFirstName(), selected_user.getLastName()));
                 username.setText(String.format("username: %s", selected_user.getUsername()));
                 email.setText(String.format("email: %s", selected_user.getEmail()));
@@ -49,15 +52,31 @@ public class ManageMembers extends JPanel{
         ActionListener delete_listener = new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                // TODO: check if valid user id ie.
-                //  not null,
-                //  user doesnt have unpaid penalties
-                //  or not returned books and user cannot delete self.
-                //new Database().removeUser(selected_user.getId());
+                // TODO: check for unpaid fines and not returned books
+                if (selected_user.getRole().equals("admin")) {
+                    deletion_prompt.setText("Cannot delete admins account!");
+                    return;
+                }
+                new Database().removeUser(selected_user.getId());
                 clear();
+                deletion_prompt.setText("User deleted successfully!");
+                selected_user = null;
             }
         };
         delete_account.addActionListener(delete_listener);
+
+        ActionListener new_password_listener = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (new_password_field.getText().isBlank()) {
+                    new_password_prompt.setText("New password cannot be empty");
+                    return;
+                }
+                new ChangeUserInfoService().ChangeUserPassword(selected_user.getUsername(), new_password_field.getText());
+                new_password_prompt.setText("Changed password successfully");
+            }
+        };
+        change_password.addActionListener(new_password_listener);
 
         clear();
     }
@@ -70,8 +89,18 @@ public class ManageMembers extends JPanel{
         email.setText("email:");
         role.setText("role:");
         delete_account.setVisible(false);
+        change_password.setVisible(false);
+        new_password_field.setVisible(false);
+        new_password_prompt.setVisible(false);
+        new_password_prompt.setText("New password");
+        deletion_prompt.setText("");
     }
 
-    // TODO: add change password text field and button
-
+    public void showComponents() {
+        user_card.setVisible(true);
+        delete_account.setVisible(true);
+        change_password.setVisible(true);
+        new_password_field.setVisible(true);
+        new_password_prompt.setVisible(true);
+    }
 }
