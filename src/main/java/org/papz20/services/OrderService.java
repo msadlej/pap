ff4179f6f58ddb20c22e52d55759acb3b39ec57c
Jakub Wróblewski <wroblewskijakub12@gmail.com>
2024-01-10 20:15:42 +0100

    space

--- src/main/java/org/papz20/services/OrderService.java
+++ src/main/java/org/papz20/services/OrderService.java
@@ -67,7 +67,6 @@ public class OrderService {
         return copies;
     }
 
-
     public boolean removeOrder(int orderID) {
         try {
             database.setOrderStatus(orderID, "hidden");
